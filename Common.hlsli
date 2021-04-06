4f8d671ce31e817c76abcce96c04f0b783ddfdbc
arcturus125 <arcturus1259@gmail.com>
2021-04-06 19:59:08 +0100

    Lab 19: starting point

---
+++ Common.hlsli
@@ -0,0 +1,94 @@
+//--------------------------------------------------------------------------------------
+// Common include file for all shaders
+//--------------------------------------------------------------------------------------
+// Using include files to define the type of data passed between the shaders
+
+
+//--------------------------------------------------------------------------------------
+// Shader input / output
+//--------------------------------------------------------------------------------------
+
+// The structure below describes the vertex data to be sent into the vertex shader.
+struct BasicVertex
+{
+    float3 position : position;
+    float3 normal   : normal;
+    float2 uv       : uv;
+};
+
+
+// This structure describes what data the lighting pixel shader receives from the vertex shader.
+// The projected position is a required output from all vertex shaders - where the vertex is on the screen
+// The world position and normal at the vertex are sent to the pixel shader for the lighting equations.
+// The texture coordinates (uv) are passed from vertex shader to pixel shader unchanged to allow textures to be sampled
+struct LightingPixelShaderInput
+{
+    float4 projectedPosition : SV_Position; // This is the position of the pixel to render, this is a required input
+                                            // to the pixel shader and so it uses the special semantic "SV_Position"
+                                            // because the shader needs to identify this important information
+    
+    float3 worldPosition : worldPosition;   // The world position and normal of each vertex is passed to the pixel...
+    float3 worldNormal   : worldNormal;     //...shader to calculate per-pixel lighting. These will be interpolated
+                                            // automatically by the GPU (rasterizer stage) so each pixel will know
+                                            // its position and normal in the world - required for lighting equations
+    
+    float2 uv : uv; // UVs are texture coordinates. The artist specifies for every vertex which point on the texture is "pinned" to that vertex.
+};
+
+
+
+// This structure is similar to the one above but for the light models, which aren't themselves lit
+struct SimplePixelShaderInput
+{
+    float4 projectedPosition : SV_Position;
+    float2 uv : uv;
+};
+
+
+//--------------------------------------------------------------------------------------
+// Constant Buffers
+//--------------------------------------------------------------------------------------
+
+// These structures are "constant buffers" - a way of passing variables over from C++ to the GPU
+// They are called constants but that only means they are constant for the duration of a single GPU draw call.
+// These "constants" correspond to variables in C++ that we will change per-model, or per-frame etc.
+
+// In this exercise the matrices used to position the camera are updated from C++ to GPU every frame along with lighting information
+// These variables must match exactly the gPerFrameConstants structure in Scene.cpp
+cbuffer PerFrameConstants : register(b0) // The b0 gives this constant buffer the number 0 - used in the C++ code
+{
+    float4x4 gViewMatrix;
+    float4x4 gProjectionMatrix;
+    float4x4 gViewProjectionMatrix; // The above two matrices multiplied together to combine their effects
+
+    float3   gLight1Position; // 3 floats: x, y z
+    float    padding1;        // Pad above variable to float4 (HLSL requirement - copied in the the C++ version of this structure)
+    float3   gLight1Colour;
+    float    padding2;
+
+    float3   gLight2Position;
+    float    padding3;
+    float3   gLight2Colour;
+    float    padding4;
+
+    float3   gAmbientColour;
+    float    gSpecularPower;
+
+    float3   gCameraPosition;
+    float    padding5;
+}
+// Note constant buffers are not structs: we don't use the name of the constant buffer, these are really just a collection of global variables (hence the 'g')
+
+
+
+// If we have multiple models then we need to update the world matrix from C++ to GPU multiple times per frame because we
+// only have one world matrix here. Because this data is updated more frequently it is kept in a different buffer for better performance.
+// We also keep other data that changes per-model here
+// These variables must match exactly the gPerModelConstants structure in Scene.cpp
+cbuffer PerModelConstants : register(b1) // The b1 gives this constant buffer the number 1 - used in the C++ code
+{
+    float4x4 gWorldMatrix;
+
+    float3   gObjectColour;
+    float    padding6;  // See notes on padding in structure above
+}
---
+++ Scene.cpp
@@ -0,0 +1,433 @@
+//--------------------------------------------------------------------------------------
+// Scene geometry and layout preparation
+// Scene rendering & update
+//--------------------------------------------------------------------------------------
+
+#include "Scene.h"
+#include "Mesh.h"
+#include "Model.h"
+#include "Camera.h"
+#include "State.h"
+#include "Shader.h"
+#include "Input.h"
+#include "Common.h"
+
+#include "CVector2.h" 
+#include "CVector3.h" 
+#include "CMatrix4x4.h"
+#include "MathHelpers.h"     // Helper functions for maths
+#include "GraphicsHelpers.h" // Helper functions to unclutter the code here
+
+#include "ColourRGBA.h" 
+
+#include <sstream>
+#include <memory>
+
+
+//--------------------------------------------------------------------------------------
+// Scene Data
+//--------------------------------------------------------------------------------------
+// Addition of Mesh, Model and Camera classes have greatly simplified this section
+// Geometry data has gone to Mesh class. Positions, rotations, matrices have gone to Model and Camera classes
+
+// Constants controlling speed of movement/rotation (measured in units per second because we're using frame time)
+const float ROTATION_SPEED = 2.0f;  // 2 radians per second for rotation
+const float MOVEMENT_SPEED = 50.0f; // 50 units per second for movement (what a unit of length is depends on 3D model - i.e. an artist decision usually)
+
+
+// Meshes, models and cameras, same meaning as TL-Engine. Meshes prepared in InitGeometry function, Models & camera in InitScene
+Mesh* gAnimatedMesh;
+Mesh* gCrateMesh;
+Mesh* gGroundMesh;
+Mesh* gLightMesh;
+
+Model* gAnimated;
+Model* gCrate;
+Model* gGround;
+
+Camera* gCamera;
+
+
+// Store lights in an array in this exercise
+const int NUM_LIGHTS = 2;
+struct Light
+{
+    Model*   model;
+    CVector3 colour;
+    float    strength;
+};
+Light gLights[NUM_LIGHTS]; 
+
+
+// Additional light information
+CVector3 gAmbientColour = { 0.2f, 0.2f, 0.3f }; // Background level of light (slightly bluish to match the far background, which is dark blue)
+float    gSpecularPower = 256; // Specular power controls shininess - same for all models in this app
+
+ColourRGBA gBackgroundColor = { 0.2f, 0.2f, 0.3f, 1.0f };
+
+// Variables controlling light1's orbiting of the cube
+const float gLightOrbit = 20.0f;
+const float gLightOrbitSpeed = 0.7f;
+
+// Lock FPS to monitor refresh rate, which will typically set it to 60fps. Press 'p' to toggle to full fps
+bool lockFPS = true;
+
+
+//--------------------------------------------------------------------------------------
+// Constant Buffers
+//--------------------------------------------------------------------------------------
+// Variables sent over to the GPU each frame
+// The structures are now in Common.h
+// IMPORTANT: Any new data you add in C++ code (CPU-side) is not automatically available to the GPU
+//            Anything the shaders need (per-frame or per-model) needs to be sent via a constant buffer
+
+PerFrameConstants gPerFrameConstants;      // The constants that need to be sent to the GPU each frame (see common.h for structure)
+ID3D11Buffer*     gPerFrameConstantBuffer; // The GPU buffer that will recieve the constants above
+
+PerModelConstants gPerModelConstants;      // As above, but constant that change per-model (e.g. world matrix)
+ID3D11Buffer*     gPerModelConstantBuffer; // --"--
+
+
+
+//--------------------------------------------------------------------------------------
+// Textures
+//--------------------------------------------------------------------------------------
+
+// DirectX objects controlling textures used in this lab
+ID3D11Resource*           gCharacterDiffuseSpecularMap    = nullptr; // This object represents the memory used by the texture on the GPU
+ID3D11ShaderResourceView* gCharacterDiffuseSpecularMapSRV = nullptr; // This object is used to give shaders access to the texture above (SRV = shader resource view)
+
+ID3D11Resource*           gCrateDiffuseSpecularMap    = nullptr;
+ID3D11ShaderResourceView* gCrateDiffuseSpecularMapSRV = nullptr;
+
+ID3D11Resource*           gGroundDiffuseSpecularMap    = nullptr;
+ID3D11ShaderResourceView* gGroundDiffuseSpecularMapSRV = nullptr;
+
+ID3D11Resource*           gLightDiffuseMap    = nullptr;
+ID3D11ShaderResourceView* gLightDiffuseMapSRV = nullptr;
+
+
+
+//--------------------------------------------------------------------------------------
+// Initialise scene geometry, constant buffers and states
+//--------------------------------------------------------------------------------------
+
+// Prepare the geometry required for the scene
+// Returns true on success
+bool InitGeometry()
+{
+    // Load mesh geometry data, just like TL-Engine this doesn't create anything in the scene. Create a Model for that.
+    // IMPORTANT NOTE: Will only keep the first object from the mesh - multipart objects will have parts missing - see later lab for more robust loader
+    try 
+    {
+        gAnimatedMesh = new Mesh("robot.x");
+        gCrateMesh    = new Mesh("CargoContainer.x");
+        gGroundMesh   = new Mesh("Hills.x");
+        gLightMesh    = new Mesh("Light.x");
+    }
+    catch (std::runtime_error e)  // Constructors cannot return error messages so use exceptions to catch mesh errors (fairly standard approach this)
+    {
+        gLastError = e.what(); // This picks up the error message put in the exception (see Mesh.cpp)
+        return false;
+    }
+
+
+    // Load the shaders required for the geometry we will use (see Shader.cpp / .h)
+    if (!LoadShaders())
+    {
+        gLastError = "Error loading shaders";
+        return false;
+    }
+
+
+    // Create GPU-side constant buffers to receive the gPerFrameConstants and gPerModelConstants structures above
+    // These allow us to pass data from CPU to shaders such as lighting information or matrices
+    // See the comments above where these variable are declared and also the UpdateScene function
+    gPerFrameConstantBuffer = CreateConstantBuffer(sizeof(gPerFrameConstants));
+    gPerModelConstantBuffer = CreateConstantBuffer(sizeof(gPerModelConstants));
+    if (gPerFrameConstantBuffer == nullptr || gPerModelConstantBuffer == nullptr)
+    {
+        gLastError = "Error creating constant buffers";
+        return false;
+    }
+
+
+    //// Load / prepare textures on the GPU ////
+
+    // Load textures and create DirectX objects for them
+    // The LoadTexture function requires you to pass a ID3D11Resource* (e.g. &gCubeDiffuseMap), which manages the GPU memory for the
+    // texture and also a ID3D11ShaderResourceView* (e.g. &gCubeDiffuseMapSRV), which allows us to use the texture in shaders
+    // The function will fill in these pointers with usable data. The variables used here are globals found near the top of the file.
+    if (!LoadTexture("MetalDiffuseSpecular.dds", &gCharacterDiffuseSpecularMap, &gCharacterDiffuseSpecularMapSRV) ||
+        !LoadTexture("CargoA.dds",               &gCrateDiffuseSpecularMap,     &gCrateDiffuseSpecularMapSRV    ) ||
+        !LoadTexture("GrassDiffuseSpecular.dds", &gGroundDiffuseSpecularMap,    &gGroundDiffuseSpecularMapSRV   ) ||
+        !LoadTexture("Flare.jpg",                &gLightDiffuseMap,             &gLightDiffuseMapSRV))
+    {
+        gLastError = "Error loading textures";
+        return false;
+    }
+
+
+  	// Create all filtering modes, blending modes etc. used by the app (see State.cpp/.h)
+	if (!CreateStates())
+	{
+		gLastError = "Error creating states";
+		return false;
+	}
+
+	return true;
+}
+
+
+// Prepare the scene
+// Returns true on success
+bool InitScene()
+{
+    //// Set up scene ////
+
+    gAnimated = new Model(gAnimatedMesh);
+    gCrate    = new Model(gCrateMesh);
+    gGround   = new Model(gGroundMesh);
+
+
+	// Initial positions
+	gAnimated->SetPosition({ 20, 0, 0 });
+    gAnimated->SetScale(5); // Increase model scale to 5.0 for the robot, which has been drawn at a different scale
+    gAnimated->SetRotation({ 0, ToRadians(135.0f), 0 });
+	gCrate-> SetPosition({ 45, 0, 45 });
+	gCrate-> SetScale(6);
+	gCrate-> SetRotation({ 0.0f, ToRadians(-50.0f), 0.0f });
+
+
+    // Light set-up - using an array this time
+    for (int i = 0; i < NUM_LIGHTS; ++i)
+    {
+        gLights[i].model = new Model(gLightMesh);
+    }
+
+    gLights[0].colour = { 0.8f, 0.8f, 1.0f };
+    gLights[0].strength = 10;
+    gLights[0].model->SetPosition({ 30, 20, 0 });
+    gLights[0].model->SetScale(pow(gLights[0].strength, 0.7f)); // Convert light strength into a nice value for the scale of the light - equation is ad-hoc.
+
+    gLights[1].colour = { 1.0f, 0.8f, 0.2f };
+    gLights[1].strength = 40;
+    gLights[1].model->SetPosition({ -20, 50, 20 });
+    gLights[1].model->SetScale(pow(gLights[1].strength, 0.7f));
+
+
+    //// Set up camera ////
+
+    gCamera = new Camera();
+    gCamera->SetPosition({ 15, 30,-70 });
+    gCamera->SetRotation({ ToRadians(13), 0, 0 });
+
+    return true;
+}
+
+
+// Release the geometry and scene resources created above
+void ReleaseResources()
+{
+    ReleaseStates();
+
+    if (gLightDiffuseMapSRV)             gLightDiffuseMapSRV->Release();
+    if (gLightDiffuseMap)                gLightDiffuseMap->Release();
+    if (gGroundDiffuseSpecularMapSRV)    gGroundDiffuseSpecularMapSRV->Release();
+    if (gGroundDiffuseSpecularMap)       gGroundDiffuseSpecularMap->Release();
+    if (gCrateDiffuseSpecularMapSRV)     gCrateDiffuseSpecularMapSRV->Release();
+    if (gCrateDiffuseSpecularMap)        gCrateDiffuseSpecularMap->Release();
+    if (gCharacterDiffuseSpecularMapSRV) gCharacterDiffuseSpecularMapSRV->Release();
+    if (gCharacterDiffuseSpecularMap)    gCharacterDiffuseSpecularMap->Release();
+
+    if (gPerModelConstantBuffer)  gPerModelConstantBuffer->Release();
+    if (gPerFrameConstantBuffer)  gPerFrameConstantBuffer->Release();
+
+    ReleaseShaders();
+
+    // See note in InitGeometry about why we're not using unique_ptr and having to manually delete
+    for (int i = 0; i < NUM_LIGHTS; ++i)
+    {
+        delete gLights[i].model;  gLights[i].model = nullptr;
+    }
+    delete gCamera;    gCamera    = nullptr;
+    delete gGround;    gGround    = nullptr;
+    delete gCrate;     gCrate     = nullptr;
+    delete gAnimated;  gAnimated = nullptr;
+
+    delete gLightMesh;     gLightMesh     = nullptr;
+    delete gGroundMesh;    gGroundMesh    = nullptr;
+    delete gCrateMesh;     gCrateMesh     = nullptr;
+    delete gAnimatedMesh;  gAnimatedMesh = nullptr;
+}
+
+
+
+//--------------------------------------------------------------------------------------
+// Scene Rendering
+//--------------------------------------------------------------------------------------
+
+// Render everything in the scene from the given camera
+void RenderSceneFromCamera(Camera* camera)
+{
+    // Set camera matrices in the constant buffer and send over to GPU
+    gPerFrameConstants.viewMatrix           = camera->ViewMatrix();
+    gPerFrameConstants.projectionMatrix     = camera->ProjectionMatrix();
+    gPerFrameConstants.viewProjectionMatrix = camera->ViewProjectionMatrix();
+    UpdateConstantBuffer(gPerFrameConstantBuffer, gPerFrameConstants);
+
+    // Indicate that the constant buffer we just updated is for use in the vertex shader (VS) and pixel shader (PS)
+    gD3DContext->VSSetConstantBuffers(0, 1, &gPerFrameConstantBuffer); // First parameter must match constant buffer number in the shader 
+    gD3DContext->PSSetConstantBuffers(0, 1, &gPerFrameConstantBuffer);
+
+
+    //// Render lit models ////
+
+    // Select which shaders to use next
+    gD3DContext->VSSetShader(gPixelLightingVertexShader, nullptr, 0);
+    gD3DContext->PSSetShader(gPixelLightingPixelShader,  nullptr, 0);
+    
+    // States - no blending, normal depth buffer and culling
+    gD3DContext->OMSetBlendState(gNoBlendingState, nullptr, 0xffffff);
+    gD3DContext->OMSetDepthStencilState(gUseDepthBufferState, 0);
+    gD3DContext->RSSetState(gCullBackState);
+
+    // Select the approriate textures and sampler to use in the pixel shader
+    gD3DContext->PSSetShaderResources(0, 1, &gGroundDiffuseSpecularMapSRV); // First parameter must match texture slot number in the shader
+    gD3DContext->PSSetSamplers(0, 1, &gAnisotropic4xSampler);
+
+    // Render model - it will update the model's world matrix and send it to the GPU in a constant buffer, then it will call
+    // the Mesh render function, which will set up vertex & index buffer before finally calling Draw on the GPU
+    gGround->Render();
+
+    // Render other lit models, only change textures for each onee
+    gD3DContext->PSSetShaderResources(0, 1, &gCharacterDiffuseSpecularMapSRV); 
+    gAnimated->Render();
+
+    gD3DContext->PSSetShaderResources(0, 1, &gCrateDiffuseSpecularMapSRV);
+    gCrate->Render();
+
+
+    //// Render lights ////
+
+    // Select which shaders to use next
+    gD3DContext->VSSetShader(gBasicTransformVertexShader, nullptr, 0);
+    gD3DContext->PSSetShader(gLightModelPixelShader,      nullptr, 0);
+
+    // Select the texture and sampler to use in the pixel shader
+    gD3DContext->PSSetShaderResources(0, 1, &gLightDiffuseMapSRV); // First parameter must match texture slot number in the shaer
+    gD3DContext->PSSetSamplers(0, 1, &gAnisotropic4xSampler);
+
+    // States - additive blending, read-only depth buffer and no culling (standard set-up for blending
+    gD3DContext->OMSetBlendState(gAdditiveBlendingState, nullptr, 0xffffff);
+    gD3DContext->OMSetDepthStencilState(gDepthReadOnlyState, 0);
+    gD3DContext->RSSetState(gCullNoneState);
+
+    // Render all the lights in the array
+    for (int i = 0; i < NUM_LIGHTS; ++i)
+    {
+        gPerModelConstants.objectColour = gLights[i].colour; // Set any per-model constants apart from the world matrix just before calling render (light colour here)
+        gLights[i].model->Render();
+    }
+}
+
+
+
+
+// Rendering the scene
+void RenderScene()
+{
+    //// Common settings ////
+
+    // Set up the light information in the constant buffer
+    // Don't send to the GPU yet, the function RenderSceneFromCamera will do that
+    gPerFrameConstants.light1Colour   = gLights[0].colour * gLights[0].strength;
+    gPerFrameConstants.light1Position = gLights[0].model->Position();
+    gPerFrameConstants.light2Colour   = gLights[1].colour * gLights[1].strength;
+    gPerFrameConstants.light2Position = gLights[1].model->Position();
+
+    gPerFrameConstants.ambientColour  = gAmbientColour;
+    gPerFrameConstants.specularPower  = gSpecularPower;
+    gPerFrameConstants.cameraPosition = gCamera->Position();
+
+
+
+    //// Main scene rendering ////
+
+    // Set the back buffer as the target for rendering and select the main depth buffer.
+    // When finished the back buffer is sent to the "front buffer" - which is the monitor.
+    gD3DContext->OMSetRenderTargets(1, &gBackBufferRenderTarget, gDepthStencil);
+
+    // Clear the back buffer to a fixed colour and the depth buffer to the far distance
+    gD3DContext->ClearRenderTargetView(gBackBufferRenderTarget, &gBackgroundColor.r);
+    gD3DContext->ClearDepthStencilView(gDepthStencil, D3D11_CLEAR_DEPTH, 1.0f, 0);
+
+    // Setup the viewport to the size of the main window
+    D3D11_VIEWPORT vp;
+    vp.Width  = static_cast<FLOAT>(gViewportWidth);
+    vp.Height = static_cast<FLOAT>(gViewportHeight);
+    vp.MinDepth = 0.0f;
+    vp.MaxDepth = 1.0f;
+    vp.TopLeftX = 0;
+    vp.TopLeftY = 0;
+    gD3DContext->RSSetViewports(1, &vp);
+
+    // Render the scene from the main camera
+    RenderSceneFromCamera(gCamera);
+
+
+    //// Scene completion ////
+
+    // When drawing to the off-screen back buffer is complete, we "present" the image to the front buffer (the screen)
+    // Set first parameter to 1 to lock to vsync (typically 60fps)
+    gSwapChain->Present(lockFPS ? 1 : 0, 0);
+}
+
+
+//--------------------------------------------------------------------------------------
+// Scene Update
+//--------------------------------------------------------------------------------------
+
+// Update models and camera. frameTime is the time passed since the last frame
+void UpdateScene(float frameTime)
+{
+	// Control sphere (will update its world matrix)
+    gAnimated->Control(0, frameTime, Key_1, Key_1, Key_1, Key_1, Key_1, Key_1, Key_J, Key_1);
+    gAnimated->Control(0, frameTime, Key_1, Key_1, Key_1, Key_1, Key_1, Key_1, Key_L, Key_1);
+    gAnimated->Control(20, frameTime, Key_1, Key_1, Key_1, Key_1, Key_J, Key_L, Key_Period, Key_Comma);
+    gAnimated->Control(8, frameTime, Key_1, Key_1, Key_1, Key_1, Key_L, Key_J, Key_Period, Key_Comma);
+
+    // Orbit the light - a bit of a cheat with the static variable [ask the tutor if you want to know what this is]
+	static float rotate = 0.0f;
+    static bool go = true;
+	gLights[0].model->SetPosition( gAnimated->Position() + CVector3{ cos(rotate) * gLightOrbit, 10, sin(rotate) * gLightOrbit } );
+    if (go)  rotate -= gLightOrbitSpeed * frameTime;
+    if (KeyHit(Key_1))  go = !go;
+
+	// Control camera (will update its view matrix)
+	gCamera->Control(frameTime, Key_Up, Key_Down, Key_Left, Key_Right, Key_W, Key_S, Key_A, Key_D );
+
+
+    // Toggle FPS limiting
+    if (KeyHit(Key_P))  lockFPS = !lockFPS;
+
+    // Show frame time / FPS in the window title //
+    const float fpsUpdateTime = 0.5f; // How long between updates (in seconds)
+    static float totalFrameTime = 0;
+    static int frameCount = 0;
+    totalFrameTime += frameTime;
+    ++frameCount;
+    if (totalFrameTime > fpsUpdateTime)
+    {
+        // Displays FPS rounded to nearest int, and frame time (more useful for developers) in milliseconds to 2 decimal places
+        float avgFrameTime = totalFrameTime / frameCount;
+        std::ostringstream frameTimeMs;
+        frameTimeMs.precision(2);
+        frameTimeMs << std::fixed << avgFrameTime * 1000;
+        std::string windowTitle = "CO2409 Week 21: Matrix Hierarchies / Animation - Frame Time: " + frameTimeMs.str() +
+                                  "ms, FPS: " + std::to_string(static_cast<int>(1 / avgFrameTime + 0.5f));
+        SetWindowTextA(gHWnd, windowTitle.c_str());
+        totalFrameTime = 0;
+        frameCount = 0;
+    }
+}
